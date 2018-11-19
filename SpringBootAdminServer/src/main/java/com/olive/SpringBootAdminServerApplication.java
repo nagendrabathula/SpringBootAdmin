a0e3aff09a8779d32ce75c531154424200fec12b
DELL <DELL@DESKTOP-B8MP7LK>
2018-11-19 17:07:12 +0530

    SpringBootAdminServer

---
+++ SpringBootAdminServer/src/main/java/com/olive/SpringBootAdminServerApplication.java
@@ -0,0 +1,33 @@
+package com.olive;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+    private static final Logger log = LoggerFactory.getLogger(SpringBootAdminServerApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+
+    @Configuration
+    public static class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()  
+                .and().csrf().disable();
+        }
+    }
+}
